5efb97dfe099f7aa537d84a395df97f6765eba07
Andy Hebrank <ahebrank@gmail.com>
2015-03-10 13:25:16 -0400

    fix the snippet

--- bugherd-snippet.php
+++ bugherd-snippet.php
@@ -1,10 +1,10 @@
-<!-- Add BugHerd Sidebar -->
-<script type='text/javascript'>
-  (function (d, t) {
-    var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
-    bh.type = 'text/javascript';
-    bh.src = '//www.bugherd.com/sidebarv2.js?apikey=${api_key}';
-    s.parentNode.insertBefore(bh, s);
-  })(document, 'script');
-</script>
+  <!-- Add BugHerd Sidebar -->
+  <script type='text/javascript'>
+    (function (d, t) {
+      var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      bh.type = 'text/javascript';
+      bh.src = '//www.bugherd.com/sidebarv2.js?apikey=<?php=$api_key?>}';
+      s.parentNode.insertBefore(bh, s);
+    })(document, 'script');
+  </script>
 </body>
