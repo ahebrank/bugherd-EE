edbcf9211a23d90a46416dd5c4e3c472c26c0fbb
Andy Hebrank <ahebrank@gmail.com>
2015-03-10 13:26:23 -0400

    fix the snippet

--- bugherd-snippet.php
+++ bugherd-snippet.php
@@ -3,7 +3,7 @@
     (function (d, t) {
       var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
       bh.type = 'text/javascript';
-      bh.src = '//www.bugherd.com/sidebarv2.js?apikey=<?=$api_key?>}';
+      bh.src = '//www.bugherd.com/sidebarv2.js?apikey=<?=$api_key?>';
       s.parentNode.insertBefore(bh, s);
     })(document, 'script');
   </script>
